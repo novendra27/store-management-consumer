5e11ee40404152728e73cece64145e99efae439c
novendra27 <adi.novendra.p@gmail.com>
2026-02-12 23:20:03 +0700

    First Commit: Initial project with depedencies and plugins

---
+++ src/test/java/javadev/project/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package javadev.project.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
